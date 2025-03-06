0038ef4da954f7f25df503cfaf47d90e3575aee5
wilfrid17 <wilfridsenatus959@gmail.com>
2025-03-05 22:25:50 -0300

    Adiciona arquivo square.py para cálculos de quadrados

---
+++ cursoOnline/square.py
@@ -0,0 +1,10 @@
+"""
+How to program in python - chopter 4
+Creating an using a program-defined fuction
+"""
+def square(number):
+    '''Defining square function'''
+    return number *number
+
+for x in range(1,11):
+    print(square(x))
